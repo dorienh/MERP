3917877a31c60254db6054f8d295778cb3d2709e
meowyan <azurnlove@hotmail.com>
2020-05-05 15:56:09 +0800

    compare feat len with actual song len

--- analysis/codes/feat_label_len_irregularity.py
+++ analysis/codes/feat_label_len_irregularity.py
@@ -42,7 +42,7 @@ def too_short(exps, feat_len_dict, threshold=20):
     qualified_idexes = []
 
     for idx, exp in exps.iterrows():
-        featlen = feat_len_dict[exp['songurl']]
+        featlen = feat_len_dict[exp['songurl']] + 12
         
         if (len(exp['arousals']) <= featlen-threshold):
             qualified_idexes.append(idx)
@@ -58,6 +58,8 @@ for idx in exps.index:
     row = exps.loc[idx]
     count += len(row['arousals'])
 print(count)
+
+print(len(tmpexps))
 count=0
 for idx in tmpexps.index:
     row = tmpexps.loc[idx]
@@ -71,5 +73,32 @@ print(count)
 ###################################################
 # openSMILE feature length vs actual song length
 ###################################################
+import scipy.io.wavfile as wav
+
+####
+# finding the actual song length in 0.1 seconds.
+####
+def gen_actual_songlen_dict():
+    actual_songlen = {}
+    songspath = "../../data/50songs"
+    songdirs = os.listdir(songspath)
+    for songdir in songdirs:
+        subpath = os.path.join(songspath,songdir,'*.wav')
+        for song in glob.glob(subpath):
+
+            (source_rate, source_sig) = wav.read(song)
+            duration_deciseconds = round(len(source_sig) / float(source_rate) *10, 2)
+
+            songname = os.path.basename(song)[:-4]
+            actual_songlen[f'{songdir}_{songname}'] = duration_deciseconds
+    return actual_songlen
+
+actual_songlen = gen_actual_songlen_dict()
+####
+#feat_len_dict vs actual_songlen...
+####
+
+len_discrepancy_dict = {key: round(actual_songlen[key] - feat_len_dict.get(key, 0),2) for key in actual_songlen}
 
 
+# %%
