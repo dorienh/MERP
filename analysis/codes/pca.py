c1c6b20f03e2fafe7106383289fe7e80a04dc271
meowyan <azurnlove@hotmail.com>
2020-12-14 00:13:24 +0800

    pca works

--- analysis/codes/pca.py
+++ analysis/codes/pca.py
@@ -20,3 +20,43 @@ cov_matrix
 values, vectors = np.linalg.eig(cov_matrix)
 values
 # %%
+
+import os
+import glob
+import pandas as pd
+import numpy as np
+import sys
+sys.path.append(os.path.abspath('../..'))
+sys.path.append(os.path.abspath('..'))
+import util
+# %%
+## 1) load feat_dict
+feat_dict = util.load_pickle('../../data/feat_dict_ready.pkl')
+# %%
+feats = list(feat_dict.values())
+# %%
+l = []
+for i in feats:
+
+    for j in i:
+        l.append(np.array(j))
+    # temp = np.array(temp)
+    # l.append(temp)
+l = np.array(l)
+# %%
+from sklearn.preprocessing import StandardScaler
+X_scaled = StandardScaler().fit_transform(l)
+# %%
+features = X_scaled.T
+cov_matrix = np.cov(features)
+cov_matrix
+# %%
+values, vectors = np.linalg.eig(cov_matrix)
+values
+# %%
+explained_variances = []
+for i in range(len(values)):
+    explained_variances.append(values[i] / np.sum(values))
+ 
+print(np.sum(explained_variances[0:500]), '\n', explained_variances[0:500])
+# %%
